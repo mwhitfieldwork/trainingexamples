07306f3d9ec775e66d5438c6104106b029aa3683
mike whitfield <mwhitfieldwork@gmail.com>
2021-12-11 12:30:06 -0600

    Add star rating validation as well as make a reactive form

--- reactiveforms/Demo-Start/src/app/customers/customer.component.ts
+++ reactiveforms/Demo-Start/src/app/customers/customer.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, NgForm } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, NgForm, ValidatorFn, Validators } from '@angular/forms';
 
 import { Customer } from './customer';
 
+
+//CUSTOM VALIDATOR
 //takes one parameter  and AbstractContorl object
 //if the the condition fails it return as key(string) and the vlaue true
 // the key is the name of the validator that you want added to the list of 
 // validation errors
 //If it passes the validation condition, then the function returns null
+
 function ratingRange (c:AbstractControl):{[key:string]: boolean} | null{
   //for this function there is a text field the looking fo r vlaues 
   //greater than 1 and less than 5
@@ -18,6 +21,33 @@ function ratingRange (c:AbstractControl):{[key:string]: boolean} | null{
 }
 
 
+//What if we need more than 1 param for the custom validator
+//we can wrap our function factor method that returns our validator ( accepts many params)
+//this function returns a validator function 
+//add fat arrow to return the validator function instead of some value
+
+/* function myValidator(param:any): ValidatorFn {
+  return (c:AbstractControl):{[key:string]: boolean} | null => {
+    for this function there is a text field the looking fo r vlaues 
+     greater than 1 and less than 5
+    if(somethingIsWrong)){
+      return {'myvalidator':true}
+    }
+    return null;
+  }
+}
+*/
+
+// Factory Validator Function Example
+function ratingRangeFactory(min:number, max:number): ValidatorFn {
+  return (c:AbstractControl):{[key:string]: boolean} | null => {
+    if(c.value !== null && (isNaN(c.value) || c.value < min || c.value > max)){
+      return {'range':true}
+    }
+    return null;
+  };
+}
+
 
 @Component({
   selector: 'app-customer',
@@ -26,14 +56,23 @@ function ratingRange (c:AbstractControl):{[key:string]: boolean} | null{
 })
 export class CustomerComponent implements OnInit {
   customer = new Customer();
+  customerForm:FormGroup;
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.customerForm = this.fb.group({
+      rating:[null,ratingRangeFactory(1,5)],
+      //cross-field validation (nested form group)
+      availability: this.fb.group({ //availability is the formGroupName in the template
+        start:['', Validators.required],//both of the names here are the formControl names in the template
+        end:['',Validators.required]
+      })
+    });
   }
 
-  save(customerForm: NgForm): void {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  save(): void {
+    //console.log(customerForm.form);
+    //console.log('Saved: ' + JSON.stringify(customerForm.value));
   }
 }
